7964fd3523959c7bf28037b3398b7326af70a0e4
DevExpressExampleBot <fakeEmail>
2021-09-02 20:28:12 +0300

    autogenerate VB

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,5 +1,4 @@
-﻿Imports Microsoft.VisualBasic
-Imports System.Windows
+﻿Imports System.Windows
 Imports System.Windows.Input
 Imports DevExpress.Xpf.Core
 Imports DevExpress.Xpf.RichEdit
@@ -7,6 +6,7 @@ Imports DevExpress.Xpf.RichEdit
 Namespace RichEditToolbarCustomCommandWpf
 	Partial Public Class MainWindow
 		Inherits Window
+
 		Public Sub New()
 			InitializeComponent()
 		End Sub
@@ -14,10 +14,12 @@ Namespace RichEditToolbarCustomCommandWpf
 
 	Public Class CustomRichEditUICommand
 		Implements ICommand
-		Private Shared ReadOnly customCommand_Renamed As New CustomRichEditUICommand("CustomCommand")
+
+'INSTANT VB NOTE: The field customCommand was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private Shared ReadOnly customCommand_Conflict As New CustomRichEditUICommand("CustomCommand")
 		Public Shared ReadOnly Property CustomCommand() As CustomRichEditUICommand
 			Get
-				Return customCommand_Renamed
+				Return customCommand_Conflict
 			End Get
 		End Property
 		Private ReadOnly commandName As String
@@ -31,7 +33,7 @@ Namespace RichEditToolbarCustomCommandWpf
 
 		#Region "ICommand Members"
 
-		Public Custom Event CanExecuteChanged As System.EventHandler Implements ICommand.CanExecuteChanged
+		Public Custom Event CanExecuteChanged As System.EventHandler
 			AddHandler(ByVal value As System.EventHandler)
 			End AddHandler
 			RemoveHandler(ByVal value As System.EventHandler)
@@ -40,16 +42,16 @@ Namespace RichEditToolbarCustomCommandWpf
 			End RaiseEvent
 		End Event
 
-		Public Function CanExecute(ByVal parameter As Object) As Boolean Implements ICommand.CanExecute
+		Public Function CanExecute(ByVal parameter As Object) As Boolean
 			Return True
 		End Function
 
-		Public Sub Execute(ByVal parameter As Object) Implements ICommand.Execute
+		Public Sub Execute(ByVal parameter As Object)
 			If commandName <> "CustomCommand" Then
 				Throw New System.ApplicationException("Unknown command")
 			End If
 
-			Dim richEditControl As RichEditControl = CType(parameter, RichEditControl)
+			Dim richEditControl As RichEditControl = DirectCast(parameter, RichEditControl)
 			DXMessageBox.Show("Number of words in the document: " & richEditControl.WordCount.ToString())
 		End Sub
 
